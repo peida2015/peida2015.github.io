4336c6065448b99a0f7cdb9e26084cde7a6e29bf
PeiDa Kuo <guo.peida@gmail.com>
2016-05-20 15:42:03 -0700

    add page indicators on buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,16 +2,51 @@
 
 (function () {
   console.log("main.js");
+  const buttonLabels = ["D3 Playground", "SciTwits", "Pong", "About Me"];
+  function highlightButtons(buttonNum) {
+    d3.selectAll('.nav-buttons').select(function () {
+      if (this.textContent === buttonLabels[buttonNum]) {
+        return this.classList.add("highlight");
+      } else {
+        return this.classList.remove('highlight')
+      };
+    });
+  };
+
   window.addEventListener("scroll", function (e) {
-    // if (window.scrollY > 400) debugger;
+
     if (window.scrollY > 400 && window.scrollY < 600) {
-      window.scrollY < 500 ? document.querySelector('a[name=d3pg]').scrollIntoView({behavior:"instant"}) : document.querySelector('a[name=index]').scrollIntoView({behavior:"instant"});
+      if (window.scrollY < 500) {
+        document.querySelector('a[name=d3pg]').scrollIntoView({behavior:"instant"});
+        highlightButtons(0);
+      } else {
+        document.querySelector('a[name=index]').scrollIntoView({behavior:"instant"});
+        d3.selectAll('.nav-buttons').classed('highlight', false);
+      }
     } else if (window.scrollY > 1000 && window.scrollY < 1200) {
-      window.scrollY < 1100 ? document.querySelector('a[name=scitwits]').scrollIntoView({behavior:"instant"}) : document.querySelector('a[name=d3pg]').scrollIntoView({behavior:"instant"})
+      if (window.scrollY < 1100) {
+         document.querySelector('a[name=scitwits]').scrollIntoView({behavior:"instant"});
+         highlightButtons(1);
+      } else {
+         document.querySelector('a[name=d3pg]').scrollIntoView({behavior:"instant"});
+         highlightButtons(0);
+      };
     } else if (window.scrollY > 1600 && window.scrollY < 1800) {
-      window.scrollY < 1700 ? document.querySelector('a[name=pong]').scrollIntoView({behavior:"instant"}) : document.querySelector('a[name=scitwits]').scrollIntoView({behavior:"instant"})
-    } else if (window.scrollY > 2200 && window.scrollY < 2400) {
-      window.scrollY < 2300 ? document.querySelector('a[name=bio]').scrollIntoView({behavior:"instant"}) : document.querySelector('a[name=pong]').scrollIntoView({behavior:"instant"})
+      if (window.scrollY < 1700) {
+         document.querySelector('a[name=pong]').scrollIntoView({behavior:"instant"});
+         highlightButtons(2)
+       } else {
+         document.querySelector('a[name=scitwits]').scrollIntoView({behavior:"instant"});
+         highlightButtons(1);
+       }
+    } else if (window.scrollY > 2300 && window.scrollY < 2500) {
+      if (window.scrollY < 2400) {
+         document.querySelector('a[name=bio]').scrollIntoView({behavior:"instant"});
+         highlightButtons(3);
+       } else {
+         document.querySelector('a[name=pong]').scrollIntoView({behavior:"instant"});
+         highlightButtons(2);
+       }
     }
   });
 })();
