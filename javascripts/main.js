3d4a8c0c2ee99c1b4c4a0ed5ffaed272580844d8
PeiDa Kuo <guo.peida@gmail.com>
2016-08-27 23:02:27 -0700

    smooth scrolling

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,52 +13,56 @@
     });
   };
 
+  var slowlyScrollTo = function (pos, dur=1500) {
+    d3.transition().duration(dur)
+    .tween('scroll', function () {
+      var itp = d3.interpolateNumber(window.scrollY, window.scrollY+pos);
+      return function (t) { scrollTo(0, itp(t)); };
+    });
+  }
+
   window.addEventListener("wheel", function (e) {
+    var indexPos = document.querySelector('a[name=index]').getBoundingClientRect().top;
 
-    var index = document.querySelector('a[name=index]');
-    var d3pg = document.querySelector('a[name=d3pg]');
-    var scitwits = document.querySelector('a[name=scitwits]');
-    var pong = document.querySelector('a[name=pong]');
-    var bio = document.querySelector('a[name=bio]');
+    var d3pgPos = document.querySelector('a[name=d3pg]').getBoundingClientRect().top;
 
-    // debugger
-    var indexPos = index.getBoundingClientRect().top;
-    var d3pgPos = d3pg.getBoundingClientRect().top;
-    var scitwitsPos = scitwits.getBoundingClientRect().top;
-    var pongPos = pong.getBoundingClientRect().top;
-    var bioPos = bio.getBoundingClientRect().top;
+    var scitwitsPos = document.querySelector('a[name=scitwits]').getBoundingClientRect().top;
 
-// debugger
-    if (d3pgPos > 200 && d3pgPos < 300) {
-      if (d3pgPos < 300 && e.deltaY > 0) {
-        d3pg.scrollIntoView({behavior:"instant"});
+    var pongPos = document.querySelector('a[name=pong]').getBoundingClientRect().top;
+
+    var bioPos = document.querySelector('a[name=bio]').getBoundingClientRect().top;
+
+
+    if (d3pgPos > 200 && d3pgPos < 400) {
+      if (d3pgPos < 400 && e.deltaY > 0) {
+        slowlyScrollTo(d3pgPos)
         highlightButtons(0);
       } else if (d3pgPos > 200 && e.deltaY < 0) {
-        document.querySelector('a[name=index]').scrollIntoView({behavior:"instant"});
+        slowlyScrollTo(indexPos)
         d3.selectAll('.nav-buttons').classed('highlight', false);
       }
-    } else if (scitwitsPos > 200 && scitwitsPos < 300) {
-      if (scitwitsPos < 300 && e.deltaY > 0) {
-         scitwits.scrollIntoView({behavior:"instant"});
+    } else if (scitwitsPos > 200 && scitwitsPos < 400) {
+      if (scitwitsPos < 400 && e.deltaY > 0) {
+         slowlyScrollTo(scitwitsPos);
          highlightButtons(1);
       } else if (scitwitsPos > 200 && e.deltaY < 0) {
-         d3pg.scrollIntoView({behavior:"instant"});
+         slowlyScrollTo(d3pgPos);
          highlightButtons(0);
       };
-    } else if (pongPos > 200 && pongPos < 300) {
-      if (pongPos < 300 && e.deltaY > 0) {
-         pong.scrollIntoView({behavior:"instant"});
+    } else if (pongPos > 200 && pongPos < 400) {
+      if (pongPos < 400 && e.deltaY > 0) {
+         slowlyScrollTo(pongPos);
          highlightButtons(2)
        } else if (pongPos > 200 && e.deltaY < 0) {
-         scitwits.scrollIntoView({behavior:"instant"});
+         slowlyScrollTo(scitwitsPos);
          highlightButtons(1);
        }
-    } else if (bioPos > 200 && bioPos < 300) {
+    } else if (bioPos > 200 && bioPos < 400) {
       if (bioPos < 300 && e.deltaY > 0) {
-         bio.scrollIntoView({behavior:"instant"});
+         slowlyScrollTo(bioPos);
          highlightButtons(3);
        } else if (bioPos > 200 && e.deltaY < 0) {
-         pong.scrollIntoView({behavior:"instant"});
+         slowlyScrollTo(pongPos);
          highlightButtons(2);
        }
     }
